fbe0b91a6c3fe23b9120062f1371cd24af94f404
EDRV02 <eduardo.diazriovarez@hotmail.com>
2024-10-24 15:02:48 +0200

    simplified code

--- UniqueSum.java
+++ UniqueSum.java
@@ -6,9 +6,11 @@ public class UniqueSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter three integers: ");
+        System.out.print("Enter a integer: ");
         int num1 = scanner.nextInt();
+        System.out.print("Enter a integer: ");
         int num2 = scanner.nextInt();
+        System.out.print("Enter a integer: ");
         int num3 = scanner.nextInt();
 
         Set<Integer> uniqueNumbers = new HashSet<>();
